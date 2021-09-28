ab33069380d42ba127e70699cd27124f0573d388
Andrew Petersen <andrew.petersen15@gmail.com>
2021-09-28 11:03:07 -0600

    added modal stories

--- src/ui-toolkit/components/Modal/Modal.stories.tsx
+++ src/ui-toolkit/components/Modal/Modal.stories.tsx
@@ -1,11 +1,6 @@
-import "./Modal.scss";
-
 import React from "react";
-
 import { Button } from "../Button/Button";
-
-import { setupStories } from "../_storybook/storybook.utils";
-import { Modal, ModalProps } from "./Modal";
+import { Modal } from "./Modal";
 
 export default {
   title: "Surfaces/Modal",
@@ -13,25 +8,43 @@ export default {
   argTypes: { children: { control: false } },
 };
 
-const { createStory } = setupStories<ModalProps>(Modal);
+export const Basic = () => {
+  let modal = Modal.useModal();
 
-export const ModalExample = createStory({
-  children: (
+  return (
     <div>
-      <div className="modal-body">
-        <p>
-          A minimum of one account owner is required to save a trusted account to your account. If
-          you would like to remove the trusted contact please confirm you would like do so;
-          otherwise, please cancel and select at least one account owner.
-        </p>
-      </div>
-      <div className="modal-footer">
-        <Button color="dark">Close</Button>
-        <Button color="success">Delete the trusted contact</Button>
-      </div>
+      <Button onClick={modal.open}>Open Modal</Button>
+      <Modal {...modal} title="Modal Title">
+        <div className="modal-body">
+          <p>I am the modal content</p>
+        </div>
+        <div className="modal-footer d-flex justify-content-end gap-2">
+          <Button variant="link" onClick={modal.close}>
+            Cancel
+          </Button>
+          <Button onClick={modal.close}>Save</Button>
+        </div>
+      </Modal>
+      <p className="mt-4">
+        The trick is to use the <code>Modal.useModal()</code> hook to supply the `props` that the
+        `Modal` component needs.
+      </p>
+
+      <p>
+        This way you have access to the <code>open()</code> and <code>close()</code> methods but
+        don't have to manage all the state yourself.
+      </p>
+      <pre>
+        {`
+let modal = Modal.useModal();
+return (
+  <Modal {...modal}>
+    ...Modal Contents
+    <button onClick={modal.close}>Cancel</button>
+  </Modal>
+)
+        `}
+      </pre>
     </div>
-  ),
-  showModal: false,
-  title: "You have deselected all of the account owners from this trusted contact",
-  onClose: close,
-});
+  );
+};
