45a74bef850769a36c492c11c1d569aa4965fada
Andrew Petersen <andrew.petersen15@gmail.com>
2022-01-21 10:50:56 -0700

    updated radio to create easily support tiles

--- src/ui-toolkit/components/forms/Radio.stories.tsx
+++ src/ui-toolkit/components/forms/Radio.stories.tsx
@@ -30,3 +30,54 @@ export const CustomLabel = createStory({
   ...Basic.args,
   children: <span className="badge bg-danger">Married</span>,
 });
+
+export const Vertical = createStory({
+  ...Basic.args,
+  orientation: "vertical",
+});
+export const HoriztonalTiles = () => {
+  return (
+    <div className="d-flex gap-2">
+      <Radio
+        className="p-3 border rounded"
+        name="martialStatus"
+        value="single"
+        orientation="vertical"
+      >
+        <span>Single</span>
+      </Radio>
+      <Radio
+        className="p-3 border rounded"
+        name="martialStatus"
+        value="married"
+        orientation="vertical"
+      >
+        Married
+      </Radio>
+      <Radio
+        className="p-3 border rounded"
+        name="martialStatus"
+        value="divorced"
+        orientation="vertical"
+      >
+        Divorced
+      </Radio>
+    </div>
+  );
+};
+
+export const VerticalTiles = () => {
+  return (
+    <div className="d-flex flex-column gap-2">
+      <Radio className="p-2 border rounded" name="martialStatus" value="single">
+        <span>Single</span>
+      </Radio>
+      <Radio className="p-2 border rounded" name="martialStatus" value="married">
+        Married
+      </Radio>
+      <Radio className="p-2 border rounded" name="martialStatus" value="divorced">
+        Divorced
+      </Radio>
+    </div>
+  );
+};
--- src/ui-toolkit/components/forms/Radio.tsx
+++ src/ui-toolkit/components/forms/Radio.tsx
@@ -4,27 +4,44 @@ import { getClassName } from "../../utils";
 
 export const Radio = React.forwardRef<HTMLInputElement, RadioProps>(
   // eslint-disable-next-line
-  function Radio({ children, value, className, id, ...rest }, ref) {
-    const cssClass = getClassName(["form-check", "align-middle", className]);
+  function Radio(
+    { children, value, className, id, style, orientation = "horizontal", ...rest },
+    ref
+  ) {
+    const cssClass = getClassName([
+      // "form-check",
+      "align-middle",
+      "d-flex align-items-center gap-2",
+      className,
+      orientation === "vertical" ? "flex-column d-inline-flex" : "",
+    ]);
     return (
-      <div className={cssClass}>
+      <label htmlFor={id || value} className={cssClass} style={style}>
         <input
           className="form-check-input"
           type="radio"
           id={id || value}
           value={value}
           ref={ref}
+          style={
+            orientation === "vertical" || true
+              ? {
+                  float: "initial",
+                  marginLeft: "0",
+                  marginTop: "0",
+                }
+              : undefined
+          }
           {...rest}
         />
-        <label className="form-check-label" htmlFor={id || value}>
-          {children}
-        </label>
-      </div>
+        {children}
+      </label>
     );
   }
 );
 export type RadioProps = React.HTMLProps<HTMLInputElement> & {
   value: string;
   children: React.ReactNode;
+  orientation?: "horizontal" | "vertical";
   className?: string;
 };
